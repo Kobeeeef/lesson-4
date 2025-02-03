505d1dbda4f183f600c9cf2ecfcbefc0a5616201
kobel <kobelei335@gmail.com>
2025-02-02 19:34:21 -0800

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Message from './components/Message';
 
 import './App.css';
@@ -6,61 +7,67 @@ import LogoImage from './secret-messages-logo.png';
 
 const apiGetAll = "http://localhost:3002/";
 const getAllParams = {
-  method: 'GET'
-}
+    method: 'GET'
+};
 
-function App() {
-  const [messages, showMessages] = useState([]);
+function App({ secrets }) {
+    const [messages, showMessages] = useState([]);
 
-  async function getMessages(isSecret) {
-    try {
-      let response = await fetch(apiGetAll + isSecret, getAllParams);
-      let receivedMessages = await response.json();
-      showMessages(receivedMessages);
-    } catch (error) {
-      console.error(error);
+    async function getMessages(isSecret) {
+        try {
+            let response = await fetch(apiGetAll + isSecret, getAllParams);
+            let receivedMessages = await response.json();
+            showMessages(receivedMessages);
+        } catch (error) {
+            console.error(error);
+        }
     }
-  }
 
-  getMessages("false");
+    useEffect(() => {
+        getMessages(secrets ? "true" : "false");
+    }, [secrets]);
 
-  return (
-      <>
-        <header>
-          <nav id="branding">
-            <img id="logo" src={LogoImage} alt="Secret Messages App" />
-            <h1 id="title">Secret Messages</h1>
-          </nav>
-        </header>
-        <div id="main">
-          <section id="messages">
-            <button
-                id="show-secret"
-                onClick={()=>getMessages("true")}
-            >
-              Show secret messages
-            </button>
-            <div className="messages-container">
-              {
-                messages.map(
-                    (message) =>
-                        <Message
-                            key={`Message${message._id}`}
-                            _id={message._id}
-                            message={message.message}
-                            user={message.user}
-                            date={message.date}
-                        />
-                )
-              }
+    return (
+        <>
+            <header>
+                <nav id="branding">
+                    <img id="logo" src={LogoImage} alt="Secret Messages App" />
+                    <h1 id="title">Secret Messages</h1>
+                </nav>
+            </header>
+            <div id="main">
+                <section id="messages">
+                    {
+                        (secrets === true) ?
+                            <Link to="/" id="show-secret" role="button">
+                                Show public messages
+                            </Link>
+                            :
+                            <Link to="/secret" id="show-secret" role="button">
+                                Show secret messages
+                            </Link>
+                    }
+                    <div className="messages-container">
+                        {
+                            messages.map(
+                                (message) =>
+                                    <Message
+                                        key={`Message${message._id}`}
+                                        _id={message._id}
+                                        message={message.message}
+                                        user={message.user}
+                                        date={message.date}
+                                    />
+                            )
+                        }
+                    </div>
+                </section>
             </div>
-          </section>
-        </div>
-        <footer>
-          <p>&copy; 2024 Secret Messages</p>
-        </footer>
-      </>
-  );
+            <footer>
+                <p>&copy; 2024 Secret Messages</p>
+            </footer>
+        </>
+    );
 }
 
 export default App;
